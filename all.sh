60a6a35e011a212f256a723fc38b227633e1cd4d
Francis Stephens <francisstephens@gmail.com>
2015-05-26 11:21:15 +0200

    Fixing missing /media/cdrom/ and sudo

--- all.sh
+++ all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get --assume-yes install build-essential module-assistant
+sudo mkdir -p /media/cdrom/
 sudo mount /dev/sr0 /media/cdrom/
 cd /media/cdrom/
-sh ./VBoxLinuxAdditions.run
+sudo sh ./VBoxLinuxAdditions.run
