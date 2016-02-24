25ebd552857c1b92873d4aa1830f40943cae6052
Francis Stephens <francisstephens@gmail.com>
2016-02-24 21:57:58 +0100

    Ensuring kernel headers are installed for guest additions

--- all.sh
+++ all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get upgrade
+
 sudo apt-get --assume-yes install build-essential module-assistant
+sudo apt-get --assume-yes install linux-headers-$(uname -r)
 sudo mkdir -p /media/cdrom/
 sudo mount /dev/sr0 /media/cdrom/
 cd /media/cdrom/
